b3f04ed0ca7fbcaa4a2e311afaaa529e48ab7359
Cesar Glufke Zanellato <cesar.g.zanellato@gmail.com>
2023-08-22 17:37:22 -0300

    Update exercicio13.C

--- Certificados/Curso de C/Aula02/exercicio13.C
+++ Certificados/Curso de C/Aula02/exercicio13.C
@@ -18,3 +18,22 @@
 // After second year: 103.022499
 // After third year: 104.544998
 
+#include <stdio.h>
+
+int main()
+{
+	float startValue = 100;
+	float interestRate = 0.015;
+	float firstYearValue;
+	float secondYearValue;
+	float thirdYearValue;
+	
+	firstYearValue = startValue + (startValue * interestRate);
+	secondYearValue = firstYearValue + (firstYearValue * interestRate);
+	thirdYearValue = secondYearValue + (firstYearValue * interestRate);
+	
+	printf("After first year: %f\n", firstYearValue);
+	printf("After second year: %f\n", secondYearValue);
+	printf("After third year: %f\n", thirdYearValue);
+	return 0;
+}
