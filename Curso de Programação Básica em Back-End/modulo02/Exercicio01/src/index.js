cdb150715f0991b865b2a503d6569b0a7b4a40e7
Cesar-Zanellato <cesar_zanellato@estudante.sesisenai.org.br>
2023-09-01 18:18:56 -0300

    Modulo02

--- Curso de Programação Básica em Back-End/modulo02/Exercicio01/src/index.js
+++ Curso de Programação Básica em Back-End/modulo02/Exercicio01/src/index.js
@@ -1,29 +1,25 @@
-/**
- * Arquivo de configuração para inicialização da aplicação.
- */
+let express = require("express");
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+const cors = require("cors");
+let Usuario = require("./modelo/usuario.js");
+const usuario = require("./modelo/usuario.js");
 
-//Importação de pacotes/módulos para uso da aplicação
-var express = require("express"); //Importação do pacote express
-var app = express(); //Inicialização da aplicação 'app' pelo pacote express
-var bodyParser = require("body-parser"); //Importação do pacote body-parser
+let app = express();
+let porta = 4000;
+let router = express.Router();
 
-/**
- * Configuração da aplicação para utilizar o pacote body-parser para retornar os dados da requisição.
- */
-app.use(bodyParser.urlencoded({ extended: true }));
+// Configuração da conexão com o MongoDB no serviço cloud MongoDB Atlas
+const uri = "mongodb+srv://Cesar:Cesar123.@cluster0.jidrbfh.mongodb.net/?retryWrites=true&w=majority";
+
+// Configuração do pacote cors para autorizar requisições de todas as origens
+app.use(cors());
+
+// Configuração da aplicação para utilizar o pacote body-parser para retornar os dados da requisição.
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//Importação do pacote cors
-const cors = require("cors");
-
-//Importação do pacote mongoose
-var mongoose = require("mongoose");
-
-//Configuração da conexão com o MongoDB no serviço cloud MongoDB Atlas
-const uri =
-  "mongodb+srv://edinilson:ed2021@cluster0.puysq.mongodb.net/?retryWrites=true&w=majority";
-
-//Validação da configuração da conexão com o MongoDB
+// Validação da configuração da conexão com MongoDB
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
@@ -36,133 +32,109 @@ mongoose
     console.log(err);
   });
 
-//Importação do arquivo de modelo que irá representar a coleção 'usuario'
-var Usuario = require("./modelo/usuario");
-
-//Definição da porta do servidor da aplicação
-var porta = 4000;
-
-//Definição da varíavel router para utilizar as instâncias das rotas do pacote express
-var router = express.Router();
-
-//Configuração do pacote cors para autorizar requisições de todas as origens
-app.use(cors());
-
-//Definição do middleware para acessar as solicitações enviadas à API
+// Definição do middleware para acessar as solicitações enviadas à API
 router.use(function (req, res, next) {
   console.log("Acesso à primeira camada do middleware...");
-  //Definição do site de origem que tem permissão de realizar a conexão com a API
-  //O "*" indicado que qualquer site pode fazer a conexão
-  res.header("Access-Control-Allow-Origin", "*");
-  //Definição dos métodos permitidos pela conexão durante o acesso à API
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  // Definição do site de origem que tem permissão de realizar a conexão com a API
+  // O "*" indicado que qualquer site pode fazer a conexão
+  res.header("Acess-Control-Allow-Origin", "*");
+  // Definição dos métodos permitidos pela conexão durante o acesso à API
+  res.header("Acess-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   app.use(cors());
   next();
 });
 
-//Rota para exibir uma mensagem ao usuário
+// Rota Inicial GET 
 app.get("/", (req, res) => {
-  res.send("Olá mundo! Esta é a página inicial da nossa aplicação.");
+  res.send("Página inicial da Aplicação.");
 });
 
-/**
- * Rota padrão para verificação do funcionamento da aplicação
- * Acesso: GET https://6j07g.sse.codesandbox.io/api
- */
-router.get("/", function (req, res) {
+// Rota padrão para verificação da aplicação. Acesso: GET https://localhost:4000/
+router.get("/", (req, res) => {
   res.json({
-    message: "Olá mundo! Está é a nossa API desenvolvida em Node.js."
+    message: "Essa API foi desenvolvida em Node.js"
   });
 });
 
-//Rotas terminadas em '/usuarios' (rotas para os verbos GET e POST)
-router
-  .route("/usuarios")
 
-  /**
-   * Método POST: cadastrar um usuário
-   * Acesso: POST https://6j07g.sse.codesandbox.io/api/usuarios
-   */
-  .post(function (req, res) {
-    var usuario = new Usuario();
-    //Definição dos campos que fazem parte da solicitação
+router
+  .route("/usuario")
+
+// Método POST: cadastrar um usuário. Acesso: POST https://localhost:4000/usuario
+  .post((req, res) => {
+    let usuario = new Usuario();
+    
     usuario.nome = req.body.nome;
     usuario.login = req.body.login;
     usuario.senha = req.body.senha;
 
     usuario.save(function (error) {
-      if (error) res.send(error);
-      res.json({ message: "Usuário cadastrado com sucesso!" });
+      if(error){ 
+        res.send(error);
+      };
+        res.json({ message: "Usuário cadastrado com sucesso!" });
     });
   })
 
-  /**
-   * Método GET: retornar a listagem de todos os usuários
-   * Acesso: GET https://6j07g.sse.codesandbox.io/api/usuarios
-   */
-  .get(function (req, res) {
-    Usuario.find(function (error, usuarios) {
-      if (error) res.send(error);
-      res.json(usuarios);
+// Método GET: retornar todos os usuários cadastrados. Acesso: GET https://localhost:4000/usuario
+  .get((req, res) => {
+    Usuario.find(function(error, usuario) {
+      if(error){
+        res.send(error);
+      }
+      res.json(usuario);
     });
   });
 
-//Rotas terminadas em '/usuarios/:usuario_id' (rotas para os verbos GET, PUT e DELETE)
+// Rota para usuario específico.
 router
-  .route("/usuarios/:usuario_id")
+  .route("/usuario/:usuario_id")
 
-  /**
-   * Método GET: listar as informações de um usuário específico
-   * Acesso: GET https://6j07g.sse.codesandbox.io/api/usuarios/:usuario_id
-   */
-  .get(function (req, res) {
-    Usuario.findById(req.params.usuario_id, function (error, usuario) {
-      if (error) res.send(error);
+// Método GET: retornar as informações de um usuário específico. Acesso: GET https://localhost:4000/usuario/:usuario_id
+  .get((req, res) => {
+    Usuario.findById(req.params.usuario_id, function (error, usuario){
+      if(error){
+        res.send(error);
+      };
       res.json(usuario);
     });
   })
 
-  /**
-   * Método PUT: atualizar as informações de um usuário específico
-   * Acesso: PUT https://6j07g.sse.codesandbox.io/api/usuarios/:usuario_id
-   */
-  .put(function (req, res) {
+// Método PUT: atualizar as informações de um usuário específico. Acesso: PUT https://localhost:4000/usuario/usuario_id
+  .put((req, res) => {
     Usuario.findById(req.params.usuario_id, function (error, usuario) {
-      if (error) res.send(error);
-      //A solicitação envia os dados para serem validados pelo esquema 'usuario'
+      if(error){
+        res.send(error);
+      };
+      
       usuario.nome = req.body.nome;
       usuario.login = req.body.login;
       usuario.senha = req.body.senha;
+
       usuario.save(function (error) {
-        if (error) res.send(error);
+        if(error){
+          res.send(error);
+        };
         res.json({ message: "Usuário atualizado com sucesso!" });
       });
     });
   })
 
-  /**
-   * Método DELETE: deletar um usuário específico
-   * Acesso: DELETE https://6j07g.sse.codesandbox.io/api/usuarios/:usuario_id
-   */
-  .delete(function (req, res) {
+// Método DELETE: deletar um usuário específico. Acesso: DELETE https://localhost:4000/usuario/usuario_id
+  .delete((req, res) => {
     Usuario.deleteOne(
       {
         _id: req.params.usuario_id
       },
       function (error) {
-        if (error) res.send(error);
-        res.json({ message: "Usuário excluído com sucesso!" });
+        if(error){
+          res.send(error);
+        };
+        res.json({ message: "Usuário excluído com sucesso!"});
       }
     );
   });
 
-
-
-  
-
 //Inicialização do servidor da aplicação
 app.listen(porta);
 console.log("Iniciando a aplicação na porta " + porta);
-
-//Definição de uma rota com prefixo '/api' para todas as rotas
-app.use("/api", router);
