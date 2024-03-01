df0ae421758bc79ac9e0a7d80ba297bccb77624c
Cesar Glufke Zanellato <cesar.g.zanellato@gmail.com>
2024-02-29 21:53:00 -0300

    Aula07

---
+++ Quarto Semestre/Prof. Rene/Aula07/Atividade02/POO/Pessoa/src/Carro.java
@@ -0,0 +1,26 @@
+public class Carro {
+    private String cor;
+    private String marca;
+    private String modelo;
+    
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    
+}
