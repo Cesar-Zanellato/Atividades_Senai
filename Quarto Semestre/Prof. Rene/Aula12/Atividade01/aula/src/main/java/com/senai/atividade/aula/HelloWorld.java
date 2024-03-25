8629fd50a1a20a9e3deb069fe84dc120d23e6571
Cesar Glufke Zanellato <cesar.g.zanellato@gmail.com>
2024-03-25 20:14:52 -0300

    Aula13 Prof Rene - BackEnd

--- Quarto Semestre/Prof. Rene/Aula12/Atividade01/aula/src/main/java/com/senai/atividade/aula/HelloWorld.java
+++ Quarto Semestre/Prof. Rene/Aula12/Atividade01/aula/src/main/java/com/senai/atividade/aula/HelloWorld.java
@@ -2,12 +2,25 @@ package com.senai.atividade.aula;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@RequestMapping("/aula")
 public class HelloWorld {
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World";
+
+    // http://localhost8081/aula?name=Nome  
+    @GetMapping
+    public String helloWorld(@RequestParam String name) {
+        return "Oie " + name;
     };
+
+    // http://localhost8081/aula/temp?celcius=0  
+    @GetMapping("/temp")
+    public String fahrenheit(@RequestParam float celcius) {
+        float fahrenheit = celcius * 9/5 + 32;
+        return "A temperatura em graus Fahrenheit é: " + fahrenheit;
+    }
+    
 };
